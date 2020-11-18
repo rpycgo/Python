d414b94e91d2ae584204b9d8cc529a7f499fd936
Jihwan Min <55495983+rluver@users.noreply.github.com>
2020-11-18 09:50:47 +0900

    Create Example 01.py

---
+++ Design Patterns/16. Template Pattern/Example 01.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+
+
+class Template(ABC):
+    
+    def __init__(self):
+        pass
+    
+    @abstractmethod
+    def function1(self):
+        pass
+    
+    @abstractmethod
+    def function2(self):
+        pass
+    
+    @staticmethod
+    def comm_fun():
+        print('Run function1 and function2')
+        
+    def execute(self):
+        self.comm_fun()
+        self.function1()
+        self.function2()
+
+
+class TemplateImplement1(Template):
+    def function1(self):
+        print('TemplateImplementation1.function1() called.')
+        
+    def function2(self):
+        print('TemplateImplementation1.function2() called.')        
+
+
+class TemplateImplement2(Template):
+    def function1(self):
+        print('TemplateImplementation2.function1() called.')
+        
+    def function2(self):
+        print('TemplateImplementation2.function2() called.')       
